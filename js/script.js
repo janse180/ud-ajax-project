6a7db5f88c72d6f20636b9eb4c819764f18080f6
Matt Jansen <janse180@umn.edu>
2017-08-02 15:11:43 -0500

    Added street view

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 
-function loadData() {
+function loadData(data) {
 
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+
+    //Convert to object notation
+    var data_obj = {};
+    data.forEach(function(element) {
+        data_obj[element.name] = element.value;
+    }, this);
+
+    console.log(data_obj);
+
+    img_src = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + data_obj.street + "," + data_obj.city 
+    
+    $greeting.text('So, you want to live at ' + data_obj.street + ", " + data_obj.city);
+    $body.append('<img class="bgimg" src="' + img_src + '">' );
 
     // clear out old data before new request
     $wikiElem.text("");
@@ -18,4 +31,7 @@ function loadData() {
     return false;
 };
 
-$('#form-container').submit(loadData);
+$('#form-container').submit(function(event) {
+    loadData($(this).serializeArray());
+    event.preventDefault();
+});
